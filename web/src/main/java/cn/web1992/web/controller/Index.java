921c2b920b598d10df5759541399101fb50514e2
web1992 <554933654@qq.com>
2019-01-17 10:58:58 +0800

    fix demo

--- web/src/main/java/cn/web1992/web/controller/Index.java
+++ web/src/main/java/cn/web1992/web/controller/Index.java
@@ -19,8 +19,11 @@ public class Index {
 
     class Greeting {
 
-        private final long id;
-        private final String content;
+        private long id;
+        private String content;
+
+        public Greeting() {
+        }
 
         public Greeting(long id, String content) {
             this.id = id;
