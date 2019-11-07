3f468fcd81631c0a4560877ccc3358998064ed6e
wangerbao <wangerbao@jiuyescm.com>
2019-11-07 22:51:29 +0800

    fix spring

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author web1992
+ */
 @SpringBootApplication
 public class DemoApplication {
 
--- web/src/main/java/cn/web1992/web/WebApplication.java
+++ web/src/main/java/cn/web1992/web/WebApplication.java
@@ -3,6 +3,9 @@ package cn.web1992.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author web1992
+ */
 @SpringBootApplication
 public class WebApplication {
 
