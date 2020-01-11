908dc1f6faba9f8463d5c222094189a073a02d9e
web1992 <554933654@qq.com>
2020-01-11 15:53:02 +0800

    fix [HomeController]

--- spring-war/src/main/java/cn/web1992/controller/HomeController.java
+++ spring-war/src/main/java/cn/web1992/controller/HomeController.java
@@ -3,12 +3,13 @@ package cn.web1992.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 
 /**
@@ -27,13 +28,7 @@ public class HomeController {
 
     private static Logger LOG = LoggerFactory.getLogger(HomeController.class);
 
-    /**
-     * @param request  request
-     * @param response response
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping(value = {"/home"}, consumes = {"application/json"})
+    @RequestMapping(value = {"/home"}, consumes = {"application/json"}, method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
     public String homeJson(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
@@ -45,5 +40,42 @@ public class HomeController {
     public String homeXml(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
         return "home xml";
+    }
+
+    /**
+     * curl -H "A:a"  127.0.0.1:8080/head
+     */
+    @RequestMapping(value = {"/head"}, headers = {"A=a"})
+    @ResponseBody
+    public String headA(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        return "head A";
+    }
+
+    /**
+     * curl -H "A:b"  127.0.0.1:8080/head
+     */
+    @RequestMapping(value = {"/head"}, headers = {"A=b"})
+    @ResponseBody
+    public String headB(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        return "head B";
+    }
+
+    /**
+     * curl -i "127.0.0.1:8080/param?a=1"
+     */
+    @RequestMapping(value = {"/param"}, params = {"a=1"})
+    @ResponseBody
+    public String param(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        return "param";
+    }
+
+    @RequestMapping(value = {"/{vv}/v"})
+    @ResponseBody
+    public String vv(HttpServletRequest request, HttpServletResponse response, @PathVariable(value = "vv") String vv) throws Exception {
+
+        return "vv is " + vv;
     }
 }
