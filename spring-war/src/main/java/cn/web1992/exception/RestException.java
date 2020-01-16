0c57c1ad7ac77656c942bdd6041c2486b434b93e
web1992 <554933654@qq.com>
2020-01-16 11:20:40 +0800

    fix [Exception]

--- spring-war/src/main/java/cn/web1992/exception/RestException.java
+++ spring-war/src/main/java/cn/web1992/exception/RestException.java
@@ -4,7 +4,7 @@ package cn.web1992.exception;
  * @author web1992
  * @date 2020/1/15  17:28
  */
-public class RestException extends Exception {
+public class RestException extends RuntimeException {
 
     public RestException(String message) {
         super(message);
--- spring-war/src/main/java/cn/web1992/exception/UnauthorizedException.java
+++ spring-war/src/main/java/cn/web1992/exception/UnauthorizedException.java
@@ -4,7 +4,7 @@ package cn.web1992.exception;
  * @author web1992
  * @date 2020/1/16  10:50
  */
-public class UnauthorizedException extends Exception {
+public class UnauthorizedException extends RuntimeException {
 
     public UnauthorizedException(String message) {
         super(message);
